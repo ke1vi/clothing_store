03f4e73b42ca32343cdb5e30f032c18900a14459
Your Name <you@example.com>
2024-10-08 15:36:30 +0300

    1

--- online_clothing_store/firstapp/views.py
+++ online_clothing_store/firstapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 def index(request):
-    return HttpResponse("Hello World! Это мой первый проект на Django!")
+    return HttpResponse("<h2>Глaвнaя</h2>")
+
+def about(request):
+    return HttpResponse("<h2>0 сайте</h2>")
+
+def contact(request):
+    return HttpResponse("<h2>Koнтaкты</h2>")
