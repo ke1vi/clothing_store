231318e242ecf46e0c9688cb2e095a2667a45f1e
Your Name <you@example.com>
2024-10-16 10:10:27 +0300

    1

--- online_clothing_store/online_clothing_store/urls.py
+++ online_clothing_store/online_clothing_store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from firstapp import views
 urlpatterns = [
-    path('', views.index, name='home'),
-    path ( 'about' , views. about),
-    path ( 'contact', views. contact),
-    ]
+    path('', views.index),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('details/', views.details),
+]
